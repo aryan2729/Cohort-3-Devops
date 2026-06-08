e665798eed5fc513ff531744309514b7cff8c181
aryan2729 <aryancyber282h@gmail.com>
2026-06-08 21:49:06 +0530

    How to ssh a monorepo project

--- week 25/25.20/packages/prisma/src/index.ts
+++ week 25/25.20/packages/prisma/src/index.ts
@@ -1,8 +1,8 @@
-import { PrismaClient } from "../generated/prisma/client/index.js";
+import { PrismaClient } from "../generated/prisma/client";
+
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const adapter = new PrismaPg({connectionString: process.env.DATABASE_URL! });
+const adapter = new PrismaPg({ connectionString : process.env.DATABASE_URL! });
 
 
-export const client = new PrismaClient( {adapter} );
-
+export const client = new PrismaClient({adapter});
