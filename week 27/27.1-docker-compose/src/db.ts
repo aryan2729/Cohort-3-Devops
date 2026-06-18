d1a859af01d89582a36125c2e838128c3639df5c
aryan2729 <aryancyber282h@gmail.com>
2026-06-18 23:40:39 +0530

    deleted

--- week 27/27.1-docker-compose/src/db.ts
+++
@@ -1,9 +0,0 @@
-import mongoose = require("mongoose")
-
-export async function connectDb() {
-    
-    await mongoose.connect("mongo://localhost:27017/week27.1");
-
-    console.log("Mongo connected")
-
-}
