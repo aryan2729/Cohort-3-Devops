26193c05d2e9e397d85b345b1e1a88cd27d7d5ef
aryan2729 <aryancyber282h@gmail.com>
2026-07-19 19:27:34 +0530

    Prometheus

--- week 31/monitor-app/index.ts
+++ week 31/monitor-app/index.ts
@@ -1,17 +1,35 @@
 import express from "express"
-import prompClient from "prom-client"
+import prometheusClient from "prom-client"
 
 import type {Request  , Response , NextFunction } from "express"
 
-
-const requestCounter = new prompClient.Counter({
+//Counter - it only can increase 
+const requestCounter = new prometheusClient.Counter({
     name: "http_requests_total",
     help: "Total number of HTTP requests",
     labelNames: ['method','route','status_code']
 });
 
+//Gauge - it can increase and decrease
+const activeRequestsGauge = new prometheusClient.Gauge({
+    name: "active_requests",
+    help: "Number of active requests"
+});
+
+
+//Histogram - it can use to observe time etc 
+const httpRequestDurationMicroseconds = new prometheusClient.Histogram({
+    name: "http_request_duration_ms",
+    help: "Duration of HTTP requests in ms",
+    labelNames: ['method','route','code'],
+    buckets: [0.1,5,15,50,100,300,500,1000,3000,5000] // define your own buckets 
+})
 
 function middleware(req: Request , res: Response , next: NextFunction){
+    
+    if (req.path !== '/metrics'){
+        activeRequestsGauge.inc();
+    }
     
     const startTime = Date.now();
 
@@ -20,11 +38,21 @@ function middleware(req: Request , res: Response , next: NextFunction){
         console.log(`Request took ${endTime - startTime}ms`);
     
 
-    requestCounter.inc({
-        method: req.method,
-        route: req.route? req.route.path:req.path,
-        status_code: req.statusCode
-    });
+        requestCounter.inc({
+            method: req.method,
+            route: req.route? req.route.path:req.path,
+            status_code: req.statusCode
+        });
+
+        if(req.route.path !== "/metrics"){
+          activeRequestsGauge.dec();
+        }
+
+        httpRequestDurationMicroseconds.observe({
+            method: req.method,
+            route: req.route ? req.route.path : req.path ,
+            code: res.statusCode,
+        }, endTime - startTime)
 
     });
     
@@ -37,11 +65,10 @@ const app = express();
 app.use(middleware); // middlware will apply to all below things | you also can add individually like app.get('/' , middleware, ( req,res)=>{})
 
 
-app.get("/cpu" ,  ( req , res)=> {
+app.get("/cpu" , async  ( req , res)=> {
+
+    await new Promise(s => setTimeout(s, Math.random() * 1000 )); // 0s to 1s
     
-    for ( let i = 0 ; i < 10000000; i++ ){
-        Math.random();
-    }
     res.json({
         message: "cpu"
     })
@@ -58,9 +85,9 @@ app.get("/users", ( req , res )=>{
 
 app.get("/metrics", async ( req,res)=>{
 
-    const metrics = await prompClient.register.metrics();
-    console.log(prompClient.register.contentType)
-    res.set('Content-Type', prompClient.register.contentType);
+    const metrics = await prometheusClient.register.metrics();
+    console.log(prometheusClient.register.contentType)
+    res.set('Content-Type', prometheusClient.register.contentType);
     res.end(metrics);
 
 })
